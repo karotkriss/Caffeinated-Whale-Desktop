2f1cbce2015b9e312f1d1f25a1b96032e5759ffd
Christopher McKay <mckay.christopher73@outlook.com>
2024-12-11 10:48:44 -0400

    fix: HydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,19 +25,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
 }
+
