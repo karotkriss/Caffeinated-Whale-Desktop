137f2b24e6d9e3af10e9cb8b9dd9e79a227e49bd
Christopher McKay <mckay.christopher73@outlook.com>
2024-12-13 17:27:37 -0400

    fix: eslint errors

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -158,10 +158,12 @@ export const MobileSidebar = ({
 export const SidebarLink = ({
   link,
   className,
+  onClick,
   ...props
 }: {
   link: Links;
   className?: string;
+  onClick?: () => void;
   props?: LinkProps;
 }) => {
   const { open, animate } = useSidebar();
@@ -172,6 +174,7 @@ export const SidebarLink = ({
         "flex items-center justify-start gap-2  group/sidebar py-2",
         className
       )}
+      onClick={onClick}
       {...props}
     >
       {link.icon}
